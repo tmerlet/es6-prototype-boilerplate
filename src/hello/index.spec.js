94244c723e577362e1b0b2010fb2ee373dc70287
Toby Merlet <tmerlet@gmail.com>
2016-09-20 01:31:41 +0100

    x skip test by default

--- src/hello/index.spec.js
+++ src/hello/index.spec.js
@@ -1,7 +1,7 @@
 import helloWorld from './index';
 import { expect } from 'chai';
 
-describe('Example test suit', () => {
+xdescribe('Example test suit', () => {
   it('should say hello world', () => {
     expect(helloWorld()).to.equal('Hello world! This is ES6 Prototype Boilerplate.');
   });
